95ac8b2f1f8c82e8a76101d4e610326b89124a4f
vo-olivier250405 <oliviervo.pro@gmail.com>
2025-07-17 14:55:00 +0200

    feat: add initial app structure with navigation and toast provider
    - Created App component as the main entry point of the application.
    - Introduced AppShell component to manage navigation using bottom tabs.
    - Added Home and Auth screens to the tab navigator.
    - Integrated React Query and Toast providers for state management and notifications.
    - Updated package.json to include ESLint and Prettier for code quality and formatting.
    - Commented out the Accept-Language header in axios interceptor for potential future use.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -20,7 +20,7 @@ apiAxios.interceptors.request.use(
         ) {
             request.headers['X-Session-Id'] = `Token ${token}`;
         }
-        request.headers['Accept-Language'] = 'fr';
+        // request.headers['Accept-Language'] = 'fr';
         return request;
     },
     (error) => Promise.reject(error)
