40ff466801e4ee869ac00bed45d04a33705f4c55
Clément <clementfiesta@gmail.com>
2025-08-10 10:25:35 +0200

    Add temp logout btn

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,11 +1,26 @@
+import { Button } from '@components/ui/button';
+import { Text } from '@components/ui/text';
+import useSignOut from '@hooks/auth/useSignOut';
+import useCurrentUser from '@hooks/users/useCurrentUser';
 import React from 'react';
-import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen: React.FC = () => {
+  const { data: user } = useCurrentUser();
+  const signOut = useSignOut();
+
+  function handleSignOut() {
+    signOut.mutate();
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex flex-col gap-4 p-8">
       <Text>Home Screen</Text>
+      {!!user && (
+        <Button onPress={handleSignOut}>
+          <Text>Sign Out</Text>
+        </Button>
+      )}
     </SafeAreaView>
   );
 };
